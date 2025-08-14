eee723ca7eb673188e7feb9c9f917b7efda51687
prtzl <matej.blagsic@protonmail.com>
2025-08-14 23:11:18 +0200

    Reorder stuff

--- profiles/system/base/pipewire.nix
+++ profiles/system/base/pipewire.nix
@@ -17,10 +17,12 @@
     enable = true;
     pulse.enable = true;
     jack.enable = true;
-    wireplumber.enable = true;
     alsa = {
       enable = true;
       support32Bit = true;
+    };
+    wireplumber = {
+      enable = true;
     };
     extraConfig.pipewire.samplerate = {
       "context.properties" = {
