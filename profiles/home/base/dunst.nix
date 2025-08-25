a38078b07a759c7a37e6fc0768aa5538619f6aa0
prtzl <matej.blagsic@protonmail.com>
2025-08-25 10:58:52 +0200

    Move dunst config to nix expression but keep original and file on hand.

--- profiles/home/base/dunst.nix
+++ profiles/home/base/dunst.nix
@@ -18,6 +18,37 @@ in
   ];
   services.dunst = {
     enable = true;
-    configFile = ./dotfiles/dunst/dunstrc;
+    # This is broken. Works on rebuild, but the file is cleaned-up after garbage collect
+    # And after service restart the file is gone and dunst breaks - goes default
+    # configFile = ./dotfiles/dunst/dunstrc;
+    settings = {
+      global = {
+        origin = "top-center";
+        alignment = "center";
+        notification_limit = 3;
+        offset = "(0, 10)";
+        separator_height = 1;
+        font = "Monospace 18";
+        width = "(0, 800)";
+        height = "(0, 300)";
+        progress_bar_height = 30;
+        format = "<b>%s - %a</b>\\n%b";
+        vertical_alignment = "center";
+        stack_duplicates = true;
+        show_indicators = false;
+      };
+
+      urgency_low = {
+        timeout = 5;
+      };
+
+      urgency_normal = {
+        timeout = 5;
+      };
+
+      urgency_critical = {
+        timeout = 5;
+      };
+    };
   };
 }
