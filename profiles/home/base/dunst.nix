8054ece9ec76456aeb9497a477ad74d08963d6a0
prtzl <matej.blagsic@protonmail.com>
2025-08-30 01:34:05 +0200

    Rice up dunst

--- profiles/home/base/dunst.nix
+++ profiles/home/base/dunst.nix
@@ -23,31 +23,66 @@ in
     # configFile = ./dotfiles/dunst/dunstrc;
     settings = {
       global = {
-        origin = "top-center";
+        # General appearance
+        font = "FiraCode Nerd Font Mono 18";
         alignment = "center";
-        notification_limit = 3;
-        offset = "(0, 10)";
-        separator_height = 1;
-        font = "Monospace 18";
-        width = "(0, 800)";
-        height = "(0, 300)";
-        progress_bar_height = 30;
-        format = "<b>%s - %a</b>\\n%b";
         vertical_alignment = "center";
-        stack_duplicates = true;
-        show_indicators = false;
+        width = "(0, 450)";
+        height = "(0, 100)";
+        offset = "(0, 40)";
+        separator_height = 2;
+        progress_bar_height = 5;
+        show_indicators = "no";
+        stack_duplicates = "yes";
+        frame_width = 5;
+        corner_radius = 18;
+        max_icon_size = 32;
+        min_icon_size = 32;
+        sticky_history = false;
+
+        # Colors;
+        frame_color = "#2E3440";
+        foreground = "#ECEFF4";
+        background = "#1A1A1A";
+        highlight = "#88C0D0";
+
+        # Icon settings;
+        icon_position = "left";
+        icon_theme = "Papirus";
+        enable_recursive_icon_lookup = true;
+
+        # Format;
+        format = "<b>%s (%a)</b>\\n%b";
+
+        # Behavior;
+        notification_limit = 5;
+        origin = "top-center";
       };
 
       urgency_low = {
-        timeout = 5;
+        timeout = 3;
+        background = "#4C566A";
+        foreground = "#D8DEE9";
       };
 
       urgency_normal = {
         timeout = 5;
+        background = "#3B4252";
+        foreground = "#ECEFF4";
       };
 
       urgency_critical = {
-        timeout = 5;
+        timeout = 10;
+        background = "#BF616A";
+        foreground = "#ECEFF4";
+        frame_color = "#800020";
+      };
+
+      volume = {
+        appname = "volume";
+        foreground = "#1bc42f";
+        frame_color = "#0d5615";
+        history_ignore = true;
       };
     };
   };
