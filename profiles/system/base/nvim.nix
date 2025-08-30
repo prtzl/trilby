956751251187dc8fc6c835b579f0692b159190b4
prtzl <matej.blagsic@protonmail.com>
2025-08-31 00:57:04 +0200

    Update nvim module ... ok

--- profiles/system/base/nvim.nix
+++ profiles/system/base/nvim.nix
@@ -4,8 +4,9 @@
 }:
 
 {
+  # using ${system} causes infinite recursion ... wot?
   imports = [
-    local.nvimnix.nixosModules.nvimnix
+    local.nvimnix.nixosModules."x86_64-linux".nvimnix
   ];
   programs.nvimnix.enable = true;
 }
